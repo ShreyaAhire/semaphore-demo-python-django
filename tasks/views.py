a54514476e152bf6ebf7915625ddb414650c0a7b
ShreyaAhire <70696874+ShreyaAhire@users.noreply.github.com>
2025-03-18 16:22:09 +0530

    Update views.py

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,11 @@
+
 """
 Task app: Views file
 """
+
 from django.views.generic import ListView, TemplateView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 
 from tasks.models import Task
 
@@ -13,67 +15,75 @@ class TaskList(ListView):
     Task list Generic List View
     """
     model = Task
-    ordering = ['-task_created']
+    ordering = ["-task_created"]
 
     def get_context_data(self, **kwargs):
-        context = super(TaskList, self).get_context_data(**kwargs)
-        context.update({'nlink': 'list'})
+        context = super().get_context_data(**kwargs)
+        context.update({"nlink": "list"})
         return context
 
 
 class TaskCreate(CreateView):
     """
-    Task list Generic Create View
+    Task Create View
     """
     model = Task
-    fields = ['task_title', 'task_description']
-    success_url = reverse_lazy('tasks:tasks_list')
+    fields = ["task_title", "task_description"]
+
+    def get_success_url(self):
+        return reverse("tasks:tasks_list")
 
     def get_context_data(self, **kwargs):
-        context = super(TaskCreate, self).get_context_data(**kwargs)
-        context.update({'nlink': 'new'})
+        context = super().get_context_data(**kwargs)
+        context.update({"nlink": "new"})
         return context
 
 
 class TaskDetails(DetailView):
     """
-    Task list Detail View
+    Task Detail View
     """
     model = Task
-    fields = ['task_title', 'task_description', 'task_created', 'task_updated']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({"nlink": "details"})
+        return context
 
 
 class TaskUpdate(UpdateView):
     """
-    Task list Update View
+    Task Update View
     """
     model = Task
-    fields = ['task_title', 'task_description']
-    success_url = reverse_lazy('tasks:tasks_list')
+    fields = ["task_title", "task_description"]
+
+    def get_success_url(self):
+        return reverse("tasks:tasks_list")
 
     def get_context_data(self, **kwargs):
-        context = super(TaskUpdate, self).get_context_data(**kwargs)
-        context.update({'nlink': 'update'})
+        context = super().get_context_data(**kwargs)
+        context.update({"nlink": "update"})
         return context
 
 
 class TaskDelete(DeleteView):
     """
-    Task list Delete View
+    Task Delete View
     """
     model = Task
-    success_url = reverse_lazy('tasks:tasks_list')
+    success_url = reverse_lazy("tasks:tasks_list")
 
 
 class Custom404(TemplateView):
     """
-    Task list Custom 404 View
+    Custom 404 View
     """
-    template_name = 'tasks/404.html'
+    template_name = "tasks/404.html"
 
 
 class Custom500(TemplateView):
     """
-    Task list Custom 500 View
+    Custom 500 View
     """
-    template_name = 'tasks/500.html'
+    template_name = "tasks/500.html"
